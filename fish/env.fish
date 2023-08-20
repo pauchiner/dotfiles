664bacb0b7a3507cf3d059b2773e04bc8b3b468c
chiner <pauchiner@icloud.com>
2023-08-20 23:58:04 +0200

    Forget to commit this files

--- fish/env.fish
+++
@@ -1,7 +0,0 @@
-# Android Studio
-export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
-export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
-
-# Cafeteria Server Enviroment variables
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/chiner/Documents/cafeteria-server/src/services/firebase-key.json";
